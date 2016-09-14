25edf7359913691af75129e0f0ab05f264fdf20c
yazgoo <yazgoo@gmail.com>
2016-09-14 22:08:46 +0200

    setting song differently

--- worker.js
+++ worker.js
@@ -5,9 +5,8 @@ pypyjs.ready().then(function() {
 onmessage = function(e) {
   console.log('Message received from main script');
   console.log(e.data[0]);
-  pypyjs.ready().then(function() {
-    return pypyjs.set('song', e.data[0])
-  }).then(function(e) {
+  pypyjs.set('song', e.data[0])
+  pypyjs.ready().then(function(e) {
     return pypyjs.execfile('mma.py')
   }).catch(function(e) {
       console.log(e);
